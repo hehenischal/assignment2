b58203bfb435f6a1139f1997758a487a73887224
hehenischal <nischallc56@gmail.com>
2023-05-07 10:51:21 +0545

    final commimt

---
+++ assignment2/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          placeholder="Add new task"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
